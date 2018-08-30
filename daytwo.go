e6810996152db0f71eec9503c2568efdea2459c1
ᕙ(⇀‸↼‶)ᕗ Crantron ᕙ(⇀‸↼‶)ᕗ <chris@gigasavvy.com>
2018-08-30 12:47:46 -0700

    first commit

---
+++ daytwo.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+)
+
+//This problem was asked by Uber.
+//Given an array of integers, return a new array such that each element at index i of the new array is the product of all the numbers in the original array except the one at i.
+//For example, if our input was [1, 2, 3, 4, 5], the expected output would be [120, 60, 40, 30, 24]. If our input was [3, 2, 1], the expected output would be [2, 3, 6].
+
+func main() {
+	ary := []int{1, 2, 3, 4, 5}
+	newAry := []int{}
+	for _, i := range ary {
+		divisor := i
+		total := 1
+
+		for _, k := range ary {
+			total *= k
+		}
+
+		newAry = append(newAry, total/divisor)
+	}
+	fmt.Printf("%v", newAry)
+}
